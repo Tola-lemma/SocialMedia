2c8ed23ddd774ec5dcc8454e2056f04acea2d85e
Tola-lemma <tolalemma@gmail.com>
2023-07-14 18:12:01 +0300

    feat:some component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,65 @@
+import { LeftBar } from "./Components/Leftbar/LeftBar";
+import { RightBar } from "./Components/Rightbar/RightBar";
+import { NavBar } from "./Components/navbar/NavBar";
+import { Profile } from "./pages/Profile/Profile";
 import { Login } from "./pages/Login/Login";
 import { RegisterPage } from "./pages/Register/Register";
-import { Route, Routes } from "react-router-dom"; 
+import { Navigate,Outlet ,RouterProvider,createBrowserRouter, } from "react-router-dom"; 
+import { Home } from "./pages/Home/Home";
 function App() {
+  const currentUser = false;
+  const Layout = ()=>{
+    return(
+      <div>
+        <NavBar />
+        <div style={{display:"flex"}}>
+           <LeftBar />
+           <Outlet />
+           <RightBar />
+        </div>
+      </div>
+    )
+  }
+
+  const ProtectedRoute = ({children})=>{
+     if(!currentUser){
+      return <Navigate to='/login'/>; 
+     } 
+     return children;
+  }
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+      <ProtectedRoute>
+        <Layout />
+      </ProtectedRoute>
+      ),
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/profile/:id",
+          element: <Profile />,
+        },
+      ],
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/register",
+      element: <RegisterPage />,
+    },
+  ]);
+
   return (
-    <Routes>
-      {/* <Route exact path="/" component={HomePage} /> */}
-      <Route path="/register" element={<RegisterPage />} />
-      <Route path="/login" element={<Login />} />
-    </Routes>
+    <div>
+      <RouterProvider router={router} />
+    </div>
   );
 }
 
---
+++ src/Components/Rightbar/RightBar.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const RightBar = () => {
+  return (
+    <div>RightBar</div>
+  )
+}
