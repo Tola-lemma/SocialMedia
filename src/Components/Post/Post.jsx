b24341f9ed8996e467de50492fea8ce588ec415d
Tola-lemma <tolalemma@gmail.com>
2023-07-16 02:04:29 +0300

    feat:like button on post is handled successfully

--- src/Components/Post/Post.jsx
+++ src/Components/Post/Post.jsx
@@ -6,13 +6,41 @@ import ShareOutlinedIcon from "@mui/icons-material/ShareOutlined";
 import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 import { Link } from "react-router-dom";
 // import Comments from "../comments/Comments";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Post = ({ post }) => {
   const [commentOpen, setCommentOpen] = useState(false);
 
   //TEMPORARY
-  const liked = false;
+  const [liked, setLiked] = useState(false);
+  const [likesCount, setLikesCount] = useState(12);
+
+  // Check localStorage for the liked status and likes count
+  useEffect(() => {
+    const likedStatus = localStorage.getItem(`post_${post.id}_liked`);
+    const likesCount = localStorage.getItem(`post_${post.id}_likesCount`);
+    if (likedStatus) {
+      setLiked(JSON.parse(likedStatus));
+    }
+    if (likesCount) {
+      setLikesCount(Number(likesCount));
+    }
+  }, [post.id]);
+  
+  // Function to handle the like action
+  const handleLike = () => {
+            if (liked) {
+              setLiked(false);
+              setLikesCount((prevCount) => prevCount - 1);
+              localStorage.setItem(`post_${post.id}_liked`, "false");
+              localStorage.setItem(`post_${post.id}_likesCount`, String(likesCount - 1));
+            } else {
+              setLiked(true);
+              setLikesCount((prevCount) => prevCount + 1);
+              localStorage.setItem(`post_${post.id}_liked`, "true");
+              localStorage.setItem(`post_${post.id}_likesCount`, String(likesCount + 1));
+            }
+  };
 
   return (
     <div className="post">
@@ -37,9 +65,9 @@ const Post = ({ post }) => {
           <img src={post.img} alt="" />
         </div>
         <div className="info">
-          <div className="item">
-            {liked ? <FavoriteOutlinedIcon /> : <FavoriteBorderOutlinedIcon />}
-            12 Likes
+          <div className="item" onClick={handleLike}>
+            {liked ? <FavoriteOutlinedIcon style={{color:"red"}}/> : <FavoriteBorderOutlinedIcon />}
+            {likesCount} Likes
           </div>
           <div className="item" onClick={() => setCommentOpen(!commentOpen)}>
             <TextsmsOutlinedIcon />
